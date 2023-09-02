f9799255782145a9d5adf31b4f1c43e3824a1ff2
Joshua Barlow <jalapenojosh@gmail.com>
2023-09-01 22:10:40 -0600

    Everything works

--- listdetailsite/src/App.tsx
+++ listdetailsite/src/App.tsx
@@ -3,9 +3,9 @@ import "./MyApp.css";
 import { Item } from "./Item";
 import { ToDoList,} from "./models/todolist";
 import { FilterInput } from "./FilterInput";
-import React from "react";
 import { TitleInput } from "./TitleInput";
 import { ContentInput } from "./ContentInput";
+import React from "react";
 
 const App = () => {
   const [items, setItems] = useState<ToDoList[]>([]);
@@ -15,6 +15,7 @@ const App = () => {
   const [addingNew, setAddingNew] = useState(false);
   const [newItemTitle, setNewItemTitle] = useState("");
   const [newItemContent, setNewItemContent] = useState("");
+  const [deleteNumber, setDeleteNumber] = useState<number | undefined>()
   useEffect(() => {
     setFilteredItems(
       items.filter((b) =>
@@ -24,50 +25,88 @@ const App = () => {
   }, [items, newFilterValue]);
 
   const addNewItem = () => {
-    const newItem: ToDoList = {
-      id: (items.length + 1),
-      title: newItemTitle,
-      content: newItemContent,
-    };
-    setNewItemContent('');
-    setNewItemTitle('');
-    setItems([...items, newItem]);
-    setAddingNew(false)
+    const lastItemId = items.length > 0 ? items[items.length - 1].id : items.length + 1;
+    if (newItemTitle.trim() !== '' && newItemContent.trim() !== '') {
+      const newItem: ToDoList = {
+        id: lastItemId + 1,
+        title: newItemTitle,
+        content: newItemContent,
+      };
+      setNewItemContent('');
+      setNewItemTitle('');
+      setItems([...items, newItem]);
+      
+    }
+    setAddingNew(false);
   };
-  
+
+  const deleteItem = () => {
+    const newList = items.filter(item => item.id != deleteNumber)
+    setItems(newList)
+  }
+
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        
-        if(addingNew === false){
-          <button onClick={() => setAddingNew(true)}>Add New Item</button>
-        }else{
-          <div>
-          <TitleInput
-          onChange={(newItemTitle) => setNewItemTitle(newItemTitle)}
-          />
-          <ContentInput
-          onChange={(newItemContent) => setNewItemContent(newItemContent)}
-          />
 
-          <button onClick={addNewItem}/>
+
+    <div>
+      {addingNew === false ? (
+        <div>
+          <div>
+            <button onClick={() =>{ 
+              setAddingNew(true)
+              setSelectedItem(undefined)}
+            }>Add New Item</button>
           </div>
-        }
+          <div>
+        <FilterInput
+        onChange={(newFilterValue) => setNewFilterValue(newFilterValue)}
+      />
+      </div>
+      <div>
+      {filteredItems.map((i) => (
+        <div>
+          <Item onClick={(b) => setSelectedItem(b)} key={i.id} item={i} />
+          </div>
+        ))} 
+        </div>
+        </div>
         
-        {selectedItem && (
+      ) : (
+        <div>
+          <div>
+          <p>Name of event</p>
+          <TitleInput
+            onChange={(newItemTitle) => setNewItemTitle(newItemTitle)}
+          />
+          </div>
+          <div>
+          <p>Description of event</p>
+          <ContentInput
+            onChange={(newItemContent) => setNewItemContent(newItemContent)}
+          />
+          </div>
+          <button onClick={addNewItem}>Add Item</button>
+        </div>
+      )}
+    </div>
+        
+    {selectedItem && (
           <div>
             <div>{selectedItem.content}</div>
-            <button>Delete Item</button>
+            <button onClick={() => 
+              {deleteItem()
+                setDeleteNumber(selectedItem.id)
+              setSelectedItem(undefined)}}>Delete Item</button>
           </div>
         )}
 
-        <FilterInput
-          onChange={(newFilterValue) => setNewFilterValue(newFilterValue)}
-        />
+       
 
-        {filteredItems.map((i) => (
-          <Item onClick={(b) => setSelectedItem(b)} key={i.id} item={i} />
-        ))}
+       
       </header>
     </div>
   );
