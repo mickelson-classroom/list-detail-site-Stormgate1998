4176e43c9ed439e9a130e1af0d543f8f01928872
Stormgate1998 <jalapenojosh@gmail.com>
2023-09-06 15:09:46 -0600

    added bootstrap

--- listdetailsite/src/index.tsx
+++ listdetailsite/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+//import "bootstrap/dist/css/bootstrap.min.css"
+import './assets/custom.scss'
 import './index.css';
 import App from './App';
 
